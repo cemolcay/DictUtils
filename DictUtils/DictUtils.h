68ceaf8178dfaadab6cab07d30a4862ff2c99ead
cemolcay <ccemolcay@gmail.com>
2019-02-08 13:16:53 -0700

    Initial Commit

---
+++ DictUtils/DictUtils.h
@@ -0,0 +1,19 @@
+//
+//  DictUtils.h
+//  DictUtils
+//
+//  Created by cem.olcay on 08/02/2019.
+//  Copyright © 2019 cemolcay. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for DictUtils.
+FOUNDATION_EXPORT double DictUtilsVersionNumber;
+
+//! Project version string for DictUtils.
+FOUNDATION_EXPORT const unsigned char DictUtilsVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <DictUtils/PublicHeader.h>
+
+
